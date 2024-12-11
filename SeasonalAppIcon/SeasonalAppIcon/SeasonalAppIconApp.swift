7994d470506047488bdff0befbe5f53acab3c63d
AyaIrshaid <a.yirshaid@gmail.com>
2024-12-11 15:47:37 +0300

    Init project

---
+++ SeasonalAppIcon/SeasonalAppIcon/SeasonalAppIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonalAppIconApp.swift
+//  SeasonalAppIcon
+//
+//  Created by Aya Irshaid on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeasonalAppIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
